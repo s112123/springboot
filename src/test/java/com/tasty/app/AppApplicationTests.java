c4ba41beff2698ffb485992ac83028902b50a3f8
s112123 <s112123@naver.com>
2024-01-23 00:14:57 +0900

    init commit

---
+++ src/test/java/com/tasty/app/AppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tasty.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
