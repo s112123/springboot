e22c7a87ab08c53c81a013abe0ed3c47420fb698
s112123 <s112123@naver.com>
2024-01-23 19:18:14 +0900

    front design 1

---
+++ src/main/java/com/tasty/app/module/main/controller/MainController.java
@@ -0,0 +1,13 @@
+package com.tasty.app.module.main.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/")
+    public String home() {
+        return "review/view";
+    }
+}
