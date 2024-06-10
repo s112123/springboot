110d8604267275c6b5b12d99880256cea413d143
s112123 <s112123@naver.com>
2024-06-10 20:23:39 +0900

    interceptor config edit

--- src/main/java/com/tasty/app/infra/config/InterceptorConfig.java
+++ src/main/java/com/tasty/app/infra/config/InterceptorConfig.java
@@ -14,7 +14,7 @@ import java.util.List;
 public class InterceptorConfig implements WebMvcConfigurer {
 
     private static List<String> excludePathsOfAuthentication = new ArrayList<>(Arrays.asList(
-            "/css/**", "/js/**",
+            "/css/**", "/js/**", "/upload/images/member/**", "/upload/images/review/**",
             "/", "/error", "/login", "/logout",
             "/reviews", "/review/view",
             "/member/add", "/members", "/members/send_email", "/members/exists_email", "/member/valid_email",
